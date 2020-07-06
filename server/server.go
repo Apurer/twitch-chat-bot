4bf9845bd3b458831c49307b92d72c27ab337405
Maciej <maciej@contact.apurer.io>
2020-07-06 22:18:10 +0200

    testing hub

--- server/server.go
+++ server/server.go
@@ -1,7 +1,7 @@
 package server 
 
 import (
-	WebSocket "github.com/Apurer/twitch-chat-bot/server/websocket"
+	//WebSocket "github.com/Apurer/twitch-chat-bot/server/websocket"
 	. "github.com/Apurer/twitch-chat-bot/server/websocket"
 	"log"
 	"flag"
@@ -13,10 +13,13 @@ var (
 	dir    = flag.String("dir", "./wwwroot", "directory to serve")
 )
 
-func Host(irc WebSocket.IRC) {
+func Host(irc IRC) {
 	fs := http.FileServer(http.Dir(*dir))
 	http.Handle("/", fs)
-	http.HandleFunc("/irc", irc.RW)
+	//http.HandleFunc("/irc", irc.RW)
+	http.HandleFunc("/irc", func(w http.ResponseWriter, r *http.Request) {
+		RWWs(hub, w, r)
+	})
 	http.HandleFunc("/echo", Echo)
 	err := http.ListenAndServeTLS(*listen, "server.rsa.crt", "server.rsa.key", nil)
     if err != nil {
--- server/websocket/websocket.go
+++ server/websocket/websocket.go
@@ -2,7 +2,6 @@ package websocket
 
 import (
 	"log"
-	"sync"
 	"net/http"
 	"github.com/gorilla/websocket"
 )
@@ -13,8 +12,8 @@ var upgrader = websocket.Upgrader{
 }
 
 type IRC struct {
-	Read chan string
-	Write chan string
+	Read *chan string
+	Write *chan string
 }
 
 func (channel *IRC) RW(w http.ResponseWriter, r *http.Request){
@@ -33,14 +32,14 @@ func (channel *IRC) RW(w http.ResponseWriter, r *http.Request){
 				break
 			}
 
-			go func() { channel.Write <- string(message) }()
+			go func() { *channel.Write <- string(message) }()
 			log.Printf("recv: %s", message)
 		}
 	}()
 
 	for {
 		select {
-		case read := <-channel.Read:
+		case read := <-*channel.Read:
 			err = c.WriteMessage(websocket.BinaryMessage, []byte(read))
 			log.Printf("write: %s", read)
 			if err != nil {
@@ -58,7 +57,7 @@ func Echo(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer c.Close()
-	
+
 	for {
 		mt, message, err := c.ReadMessage()
 		if err != nil {
